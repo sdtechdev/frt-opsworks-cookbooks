a07b7be9f295d53494a39c1a76da260862637952
Shkrt <zxcgpppmnn@gmail.com>
2018-10-18 14:24:42 +0700

    Upgrade to 2.3.8

--- ruby/attributes/customize.rb
+++ ruby/attributes/customize.rb
@@ -2,7 +2,7 @@ case node['opsworks']['ruby_version']
 when '2.3'
   normal[:ruby][:major_version] = '2'
   normal[:ruby][:minor_version] = '3'
-  normal[:ruby][:patch_version] = '7'
+  normal[:ruby][:patch_version] = '8'
   normal[:ruby][:pkgrelease]    = '1'
 
   normal[:ruby][:full_version] = [node[:ruby][:major_version],
